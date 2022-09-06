3c44c717cc36869684055d0f3ba8f8490a6d3fe7
m.sofiichuk <koyap@ukr.net>
2022-09-06 04:43:44 +0300

    check let-const and func-class

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+let Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -24,9 +24,28 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player=function(x, y){
+    this.sprite='images/char-boy.png';
+    this.x=x;
+    this.y=y;
+};
+Player.prototype.update=function(){
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+let player = new Player(200, 385);
+// class Player {
+//     constructor{}
+// }
 
 
 // Now instantiate your objects.
+let enemy1 = new Enemy;
+const allEnemies = [enemy1];
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
@@ -35,7 +54,7 @@ Enemy.prototype.render = function() {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
